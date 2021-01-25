61d95033a014a807c29966acfe615c2f012f32ab
alexandredev3 <costasalexandre511@gmail.com>
2021-01-25 20:57:00 -0300

    Aula 01 Concluída

---
+++ pages/_app.tsx
@@ -0,0 +1,51 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import db from '../db.json';
+
+interface Props {
+  Component: () => null;
+  pageProps: any;
+}
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+
+  html, body {
+    min-height: 100vh;
+  }
+
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const theme = db.theme;
+
+export default function App({ Component, pageProps }: Props) {
+  return (
+    <>
+      <ThemeProvider
+        theme={theme}
+      >
+        <GlobalStyle />
+        <Component 
+          { ...pageProps }
+        />
+      </ThemeProvider>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,53 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+
+const BackgroundImage = styled.div`
+  flex: 1;
+  background-image: url(${db.bg});
+  background-position: center;
+  background-size: cover;
+`;
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz CSS da Alura</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, minus.</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, minus.</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+
+      <GitHubCorner projectUrl="https://github.com/alexandredev3" />
+    </QuizBackground>
+  )
 }
